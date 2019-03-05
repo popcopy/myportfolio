bae0be411cab196c363478ad4904c73468a5d36a
Matt Urian <matt.a.urian@gmail.com>
2019-03-05 17:35:55 -0500

    update

--- assets/JavaScript/crystal.js
+++ assets/JavaScript/crystal.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     }
     function randomGemGen() {
         greenGemNum = Math.floor(Math.random() * 11) + 1;
-        blueGemNum = Math.floor(Math.random() * 11) + 1; 
+        blueGemNum = Math.floor(Math.random() * 11) + 1;
         goldGemNum = Math.floor(Math.random() * 11) + 1;
         purpleGemNum = Math.floor(Math.random() * 11) + 1;
     }
@@ -24,95 +24,95 @@ $(document).ready(function () {
     var wins = 0;
     var losses = 0;
 
-$("#greenGem").on("click", function () {
-    yourScoreIs = yourScoreIs + greenGemNum;
-    $("#yourScore").html(yourScoreIs);
+    $("#greenGem").on("click", function () {
+        yourScoreIs = yourScoreIs + greenGemNum;
+        $("#yourScore").html(yourScoreIs);
         if (yourScoreIs === randomNumber) {
             wins = wins + 1;
             $("#wins").html("Wins:" + wins);
-            $("#message").html("YOU WIN!"); 
-        randomNumGen();
-        randomGemGen();
-        yourScoreIs = 0;
-        $("#yourScore").html(yourScoreIs);
-    }
+            $("#message").html("YOU WIN!");
+            randomNumGen();
+            randomGemGen();
+            yourScoreIs = 0;
+            $("#yourScore").html(yourScoreIs);
+        }
         if (yourScoreIs > randomNumber) {
             losses = losses + 1;
             $("#losses").html("Losses:" + losses);
             $("#message").html("YOU LOSE!");
-        randomNumGen();
-        randomGemGen();
-        yourScoreIs = 0;
-        $("#yourScore").html(yourScoreIs);
-    }
-});
-$("#blueGem").on("click", function () {
+            randomNumGen();
+            randomGemGen();
+            yourScoreIs = 0;
+            $("#yourScore").html(yourScoreIs);
+        }
+    });
+    $("#blueGem").on("click", function () {
         yourScoreIs = yourScoreIs + blueGemNum;
         $("#yourScore").html(yourScoreIs);
         if (yourScoreIs === randomNumber) {
             wins = wins + 1;
             $("#wins").html("Wins:" + wins);
-            $("#message").html("YOU WIN!"); 
-        randomNumGen();
-        randomGemGen();
-        yourScoreIs = 0;
-        $("#yourScore").html(yourScoreIs);
-    }
+            $("#message").html("YOU WIN!");
+            randomNumGen();
+            randomGemGen();
+            yourScoreIs = 0;
+            $("#yourScore").html(yourScoreIs);
+        }
         if (yourScoreIs > randomNumber) {
             losses = losses + 1;
             $("#losses").html("Losses:" + losses);
             $("#message").html("YOU LOSE!");
-        randomNumGen();
-        randomGemGen();
-        yourScoreIs = 0;
-        $("#yourScore").html(yourScoreIs);
-    }
-});
-$("#goldGem").on("click", function () {
+            randomNumGen();
+            randomGemGen();
+            yourScoreIs = 0;
+            $("#yourScore").html(yourScoreIs);
+        }
+    });
+    $("#goldGem").on("click", function () {
         yourScoreIs = yourScoreIs + goldGemNum;
         $("#yourScore").html(yourScoreIs);
         if (yourScoreIs === randomNumber) {
             wins = wins + 1;
             $("#wins").html("Wins:" + wins);
             $("#message").html("YOU WIN!");
-        randomNumGen();
-        randomGemGen();
-        yourScoreIs = 0;
-        $("#yourScore").html(yourScoreIs);
-    }
+            randomNumGen();
+            randomGemGen();
+            yourScoreIs = 0;
+            $("#yourScore").html(yourScoreIs);
+        }
         if (yourScoreIs > randomNumber) {
             losses = losses + 1;
             $("#losses").html("Losses:" + losses);
             $("#message").html("YOU LOSE!");
-        randomNumGen();
-        randomGemGen();
-        yourScoreIs = 0;
-        $("#yourScore").html(yourScoreIs);
-    }
-});
-$("#purpleGem").on("click", function () {
+            randomNumGen();
+            randomGemGen();
+            yourScoreIs = 0;
+            $("#yourScore").html(yourScoreIs);
+        }
+    });
+    $("#purpleGem").on("click", function () {
         yourScoreIs = yourScoreIs + purpleGemNum;
         $("#yourScore").html(yourScoreIs);
         if (yourScoreIs === randomNumber) {
             wins = wins + 1;
             $("#wins").html("Wins:" + wins);
             $("#message").html("YOU WIN!");
-        randomNumGen();
-        randomGemGen();
-        yourScoreIs = 0;
-        $("#yourScore").html(yourScoreIs);
-    }
+            randomNumGen();
+            randomGemGen();
+            yourScoreIs = 0;
+            $("#yourScore").html(yourScoreIs);
+        }
         if (yourScoreIs > randomNumber) {
             losses = losses + 1;
             $("#losses").html("Losses:" + losses);
             $("#message").html("YOU LOSE!");
-        randomNumGen();
-        randomGemGen();
-        yourScoreIs = 0;
-        $("#yourScore").html(yourScoreIs);
-    }
-});
-$("#reset").on("click", function () {
+            randomNumGen();
+            randomGemGen();
+            yourScoreIs = 0;
+            $("#yourScore").html(yourScoreIs);
+        }
+    });
+    $("#reset").on("click", function () {
         wins = 0;
         losses = 0;
         yourScoreIs = 0;
